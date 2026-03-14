4dea85c6f53945a5504f211ef684ceb5d41bfe08
MENZSHOPS-Div <menzshopsaddress@gmail.com>
2026-03-14 08:47:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,34 +4,21 @@ const app = express()
 
 app.use(express.json())
 
-const TOKEN = "fG9weBficKjxjm+Xl4xu5fbcz/xtSAnmpjzZyCpBEKEvIx+tOH4pAzBD8s1ZuoCtRhyejCrOF92ty2hlj7Z0qUdAS8rtfvAyNrcHXV+BMF2XMNOnR8smAG+10JoMBDHh5BuZf1eXgHygymAssyhzXQdB04t89/1O/w1cDnyilFU="
+app.post("/webhook", (req, res) => {
 
-app.post("/webhook", async (req,res)=>{
+  console.log("LINE EVENT:")
+  console.log(JSON.stringify(req.body, null, 2))
 
-    const event = req.body.events[0]
+  res.status(200).send("OK")
 
-    if(event && event.type === "message"){
+})
 
-        const replyToken = event.replyToken
-        const text = event.message.text
-
-        await fetch("https://api.line.me/v2/bot/message/reply",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer " + TOKEN
-            },
-            body:JSON.stringify({
-                replyToken:replyToken,
-                messages:[{type:"text",text:"คุณพิมพ์ว่า: "+text}]
-            })
-        })
-
-    }
-
-    res.status(200).send("OK")
+app.get("/", (req, res) => {
+  res.send("LINE webhook running")
 })
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>console.log("server started"))
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
